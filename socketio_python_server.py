a88ffcf1d0d409b4401d2b12b66e672f6f9cdc2c
Alton Panton <alton.panton@gmail.com>
2019-08-18 20:32:59 -0400

    changed some stuff

--- socketio_python_server.py
+++ socketio_python_server.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-sockets = Sockets(app)
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    send(message)
 
-@sockets.route('/')
-def echo_socket(ws):
-    while True:
-        message = ws.receive()
-        ws.send(message[::-1])
+# @socketio.route('/')
+# def echo_socket(ws):
+#     while True:
+#         message = ws.receive()
+#         ws.send(message[::-1])
 
 # @app.route('/')
 # def hello():
 #     return 'Hello World!'
 
-@app.route('/echo_test', methods=['GET'])
-def echo_test():
-    return render_template('echo_test.html')
+# @app.route('/echo_test', methods=['GET'])
+# def echo_test():
+#     return render_template('echo_test.html')
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
--- test.py
+++ test.py
@@ -8,6 +8,8 @@ red.off()
 
 sio = socketio.Client()
 
+sio.emit('message',{'data':'off'})
+
 @sio.event
 def connect():
     print('connection established')
@@ -22,8 +24,12 @@ def message(data):
     sio.emit('data', data[::-1])
     if data == "on":
         red.on()
+        print(data)
     elif data == "off":
         red.off()
+        print(data)
+    else:
+        print(data)
 
 sio.connect('http://localhost:6969')
 sio.wait()
