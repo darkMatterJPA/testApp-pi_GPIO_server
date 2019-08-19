f0a850b2c8b30624e6b117929485c81edc33eced
Alton Panton <alton.panton@gmail.com>
2019-08-19 00:59:50 -0400

    added the java socketIO interface from git hub
    https://github.com/Gottox/socket.io-java-client

--- test_LED_IoT/app/src/main/java/com/example/test_led_iot/MainActivity.java
+++ test_LED_IoT/app/src/main/java/com/example/test_led_iot/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.test_led_iot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.MalformedURLException;
+
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,18 +30,65 @@ public class MainActivity extends AppCompatActivity {
         sw.setTextOn("ON");
         sw.setChecked(false);
 
-        final NetCom com = new NetCom("192.168.1.118",6969);
+       // final NetCom com = new NetCom("192.168.1.118",6969);
+        SocketIO socket = null;
+        try {
+            socket = new SocketIO("http://192.168.1.115:5000/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        socket.connect(new IOCallback() {
+            @Override
+            public void onMessage(JSONObject json, IOAcknowledge ack) {
+//                try {
+//                    System.out.println("Server said:" + json.toString(2));
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+            }
+
+            @Override
+            public void onMessage(String data, IOAcknowledge ack) {
+                Log.i("INFO","Server said: " + data);
+            }
+
+            @Override
+            public void onError(SocketIOException socketIOException) {
+               Log.e("ERROR","an Error occured: "+socketIOException.getMessage());
 
 
+            }
+
+            @Override
+            public void onDisconnect() {
+                Log.i("INFO","Connection terminated.");
+            }
+
+            @Override
+            public void onConnect() {
+               // System.out.println("Connection established");
+                Log.i("INFO","Connection established");
+            }
+
+            @Override
+            public void on(String event, IOAcknowledge ack, Object... args) {
+                Log.i("INFO","Server triggered event '" + event + "'");
+            }
+        });
+
+        final SocketIO finalSocket = socket;
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
              public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                  String toast;
                  if (isChecked) {
                      // The toggle is enabled
-                      toast = com.sendMsg("On");
+                     finalSocket.send("On");
+                      toast = "On"; //com.sendMsg("On");
                  } else {
                      // The toggle is disabled
-                        toast = com.sendMsg("Off");
+                     finalSocket.send("Off");
+                        toast = "Off"; //com.sendMsg("Off");
                  }
                  Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
              }
