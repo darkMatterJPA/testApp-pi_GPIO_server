6ccfde6318ed3c3d961bc913df5de78ebe05f132
Alton Panton <alton.panton@gmail.com>
2019-08-19 23:21:18 -0400

    getting rid of the socket IO stuff it doesn't work =(
    I removed the socket IO jar file and removed the socket IO object code. the gradle would not build with the jar file. Gradle build was successful at this point.

--- test_LED_IoT/app/src/main/java/com/example/test_led_iot/MainActivity.java
+++ test_LED_IoT/app/src/main/java/com/example/test_led_iot/MainActivity.java
@@ -12,10 +12,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import io.socket.IOAcknowledge;
-import io.socket.IOCallback;
-import io.socket.SocketIO;
-import io.socket.SocketIOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,69 +23,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sw = findViewById(R.id.switch1);
-        sw.setTextOff("OFF");
-        sw.setTextOn("ON");
         sw.setChecked(false);
 
        // final NetCom com = new NetCom("192.168.1.118",6969);
-        SocketIO socket = null;
-        try {
-            socket = new SocketIO("http://192.168.1.115:5000/");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
 
-        socket.connect(new IOCallback() {
-            @Override
-            public void onMessage(JSONObject json, IOAcknowledge ack) {
-//                try {
-//                    System.out.println("Server said:" + json.toString(2));
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-            }
-
-            @Override
-            public void onMessage(String data, IOAcknowledge ack) {
-                Log.i("INFO","Server said: " + data);
-                Toast.makeText(getApplicationContext(), "Server said: " + data, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(SocketIOException socketIOException) {
-               Log.e("ERROR","an Error occurred: "+socketIOException.getMessage());
-               Toast.makeText(getApplicationContext(), "an Error occurred: "+socketIOException.getMessage(), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onDisconnect() {
-                Log.i("INFO","Connection terminated.");
-            }
-
-            @Override
-            public void onConnect() {
-                Log.i("INFO","Connection established");
-                Toast.makeText(getApplicationContext(), "Connection established", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void on(String event, IOAcknowledge ack, Object... args) {
-                Log.i("INFO","Server triggered event '" + event + "'");
-            }
-        });
-
-        final SocketIO finalSocket = socket;
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
              public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                  String toast;
                  if (isChecked) {
                      // toggle is on
-                     finalSocket.send("On");
+
                       toast = "On";
                  } else {
                      // toggle is off
-                     finalSocket.send("Off");
+
                         toast = "Off";
                  }
                  Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
